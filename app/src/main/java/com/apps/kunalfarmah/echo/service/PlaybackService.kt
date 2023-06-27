0e3e92a994f2d4a115d57dd80b98b7e0cd238307
Kunal Farmah <kunalfarmah98@gmail.com>
2023-06-27 21:16:18 +0530

    fixed order of custom commands and enabled shuffle toggle in <13

--- app/src/main/java/com/apps/kunalfarmah/echo/service/PlaybackService.kt
+++ app/src/main/java/com/apps/kunalfarmah/echo/service/PlaybackService.kt
@@ -55,6 +55,51 @@ class PlaybackService : MediaSessionService(), MediaSession.Callback {
                     .setDisplayName("close")
                     .build()
     )
+
+    var customCommandsLegacy = listOf(
+            CommandButton.Builder().setSessionCommand(
+                    SessionCommand(ShuffleActions.CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF.name, Bundle()))
+                    .setIconResId(
+                            R.drawable.baseline_shuffle_24
+                    )
+                    .setEnabled(true)
+                    .setDisplayName("shuffle on")
+                    .build(),
+            CommandButton.Builder().setSessionCommand(
+                    SessionCommand(ShuffleActions.CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF.name, Bundle()))
+                    .setIconResId(R.drawable.baseline_shuffle_on_24)
+                    .setEnabled(true)
+                    .setDisplayName("shuffle off")
+                    .build(),
+            CommandButton.Builder().setPlayerCommand(Player.COMMAND_SEEK_TO_PREVIOUS)
+                    .setIconResId(android.R.drawable.ic_media_previous)
+                    .setEnabled(true)
+                    .setDisplayName("previous")
+                    .build(),
+            CommandButton.Builder().setPlayerCommand(Player.COMMAND_PLAY_PAUSE)
+                    .setIconResId(
+                            if(mediaPlayer.isPlaying)
+                                android.R.drawable.ic_media_pause
+                            else
+                                android.R.drawable.ic_media_play
+                    )
+                    .setEnabled(true)
+                    .setDisplayName("play/pause")
+                    .build(),
+            CommandButton.Builder().setPlayerCommand(Player.COMMAND_SEEK_TO_NEXT_MEDIA_ITEM)
+                    .setIconResId(android.R.drawable.ic_media_next)
+                    .setEnabled(true)
+                    .setDisplayName("next")
+                    .build(),
+
+            CommandButton.Builder().setSessionCommand(
+                    SessionCommand("action_close", Bundle()))
+                    .setIconResId(R.drawable.baseline_close_24)
+                    .setEnabled(true)
+                    .setDisplayName("close")
+                    .build()
+
+    )
     enum class ShuffleActions(name: String) {
         CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_ON("action_shuffle_on"),
         CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF("action_shuffle_on")
@@ -80,14 +125,25 @@ class PlaybackService : MediaSessionService(), MediaSession.Callback {
                 .setCallback(this)
                 .build()
 
-        customLayout = if(MediaUtils.isShuffle){
-            listOf(customCommands[1],customCommands[2])
+        mediaSession?.setCustomLayout(getCustomLayout())
+        super.onCreate()
+    }
+
+    private fun getCustomLayout(): List<CommandButton> {
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (MediaUtils.isShuffle) {
+                 listOf(customCommands[1], customCommands[2])
+            } else {
+                listOf(customCommands[0], customCommands[2])
+            }
         }
         else{
-            listOf(customCommands[0],customCommands[2])
+            if (MediaUtils.isShuffle) {
+                listOf(customCommandsLegacy[1], customCommandsLegacy[2], customCommandsLegacy[3], customCommandsLegacy[4], customCommandsLegacy[5])
+            } else {
+                listOf(customCommandsLegacy[0], customCommandsLegacy[2], customCommandsLegacy[3], customCommandsLegacy[4], customCommandsLegacy[5])
+            }
         }
-        mediaSession?.setCustomLayout(customLayout)
-        super.onCreate()
     }
 
     // Return a MediaSession to link with the MediaController that is making
@@ -98,12 +154,7 @@ class PlaybackService : MediaSessionService(), MediaSession.Callback {
 
     fun setCustomLayoutForShuffle(isShuffle: Boolean?){
         mediaPlayer.shuffleModeEnabled = (isShuffle == true)
-        customLayout = if (isShuffle == true) {
-            listOf(customCommands[1], customCommands[2])
-        } else {
-            listOf(customCommands[0], customCommands[2])
-        }
-        mediaSession?.setCustomLayout(customLayout)
+        mediaSession?.setCustomLayout(getCustomLayout())
     }
 
     override fun onDestroy() {
@@ -143,15 +194,15 @@ class PlaybackService : MediaSessionService(), MediaSession.Callback {
 //            session.player.shuffleModeEnabled = true
             SongPlayingFragment.Statified.shufflebutton.callOnClick()
             // Change the custom layout to contain the `Disable shuffling` command and send the updated custom layout to controllers.
-            customLayout = listOf(customCommands[1], customCommands[2])
-            session.setCustomLayout(customLayout)
+            //customLayout = listOf(customCommands[1], customCommands[2])
+            session.setCustomLayout(getCustomLayout())
         } else if (ShuffleActions.CUSTOM_COMMAND_TOGGLE_SHUFFLE_MODE_OFF.name == customCommand.customAction) {
             // Disable shuffling.
 //            session.player.shuffleModeEnabled = false
             SongPlayingFragment.Statified.shufflebutton.callOnClick()
             // Change the custom layout to contain the `Enable shuffling` command and send the updated custom layout to controllers.
-            customLayout = listOf(customCommands[0], customCommands[2])
-            session.setCustomLayout(customLayout)
+           // customLayout = listOf(customCommands[0], customCommands[2])
+            session.setCustomLayout(getCustomLayout())
         }
         else if(customCommand.customAction == "action_close" ){
             killApp()
@@ -162,7 +213,7 @@ class PlaybackService : MediaSessionService(), MediaSession.Callback {
     override fun onPostConnect(session: MediaSession, controller: MediaSession.ControllerInfo) {
         if (customLayout.isNotEmpty()) {
             // Let the controller now about the custom layout right after it connected.
-            mediaSession?.setCustomLayout(controller, customLayout)
+            mediaSession?.setCustomLayout(controller, getCustomLayout())
         }
     }
 
