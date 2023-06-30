ac8e534c8d7400159085165b2d91280f22f51a1e
Kunal Farmah <kunalfarmah98@gmail.com>
2023-07-01 02:45:15 +0530

    stopped receiving playback commands when mediaSession is inactive

---
+++ app/src/main/java/com/apps/kunalfarmah/echo/receiver/MediaButtonReceiver.kt
@@ -0,0 +1,9 @@
+package com.apps.kunalfarmah.echo.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class EchoMediaButtonReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {}
+}
